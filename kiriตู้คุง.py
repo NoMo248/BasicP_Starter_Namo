db3c8121e2e8dcfb7691d77b15715f5e90210814
NoMo248 <teeraphatnamo555@gmail.com>
2025-07-23 13:36:37 +0700

    Add files via upload

--- kiriตู้คุง.py
+++ kiriตู้คุง.py
@@ -1,25 +1,41 @@
 monster = 43172
-sword = monster - 21.5
-AmericanStyle = monster - 86
+stareintoitsoul = 1
+sword = 21.5
+AmericanStyle = 86
 holyminecraftstick = 21586
 print("สู้ป่าวๆๆๆๆๆ กลัวรึป่าวๆๆๆ กด 1 สู้ 2 หนี ")
 x = int(input("1 or 2 ตอบ "))
 if x == 1:
-    print("ดีมากที่เลือกสู้ เริ่มก่อนเลยจากการเลือกอาวุธก่อน มี 1.sword 2.AmericanStyle 3.holyminecraftstick แต่ละอาวุธดาเมจไม่เท่ากันเลือก ")
+    print("ดีมากที่เลือกสู้ เริ่มก่อนเลยจากการเลือกอาวุธก่อน มี 1.stareintoitsoul (emotional damage) 2.sword 3.AmericanStyle 4.holyminecraftstick แต่ละอาวุธดาเมจไม่เท่ากันเลือก ")
+elif x == 2:
+    print("nuh uh หนีไม่รอด GGez สู้อยู่ดีบังคับ เริ่มก่อนเลยจากการเลือกอาวุธก่อน มี 1.stareintoitsoul (emotional damage) 2.sword 3.AmericanStyle 4.holyminecraftstick แต่ละอาวุธดาเมจไม่เท่ากันเลือก ")
 else:
-    print("nuh uh หนีไม่รอด GGez สู้อยู่ดีบังคับ เริ่มก่อนเลยจากการเลือกอาวุธก่อน มี 1.sword 2.AmericanStyle 3.holyminecraftstick แต่ละอาวุธดาเมจไม่เท่ากันเลือก ")
+    print("หนีไปไหนไม่รอดหรอกกลับไปเลือก")
 weapon = int(input("อาวุธเลข??? "))
 print(weapon)
 print("จะตีกี่รอบ??? คิดดีดีนะ เพราะถ้าไม่ตีให้มันตายพอดี มันจะเกิดใหม่")
 num = int(input("ตีกี่รอบจ๊ะ "))
-weapon1 = 21.5
-weapon2 = 86
-weapon3 = 21586
+weapon1 = 1
+weapon2 = 21.5
+weapon3 = 86
+weapon4 = 21586
 
 i = 1
 while i <= num:
-    monster = monster - weapon1
-    print(f"ครั้งที่ {i} hp left", monster)
+    if weapon == 1:
+        monster = monster - weapon1
+        print(f"ครั้งที่ {i} hp left", monster)
+    elif weapon == 2:
+        monster = monster - weapon2
+        print(f"ครั้งที่ {i} hp left", monster)
+    elif weapon == 3:
+        monster = monster - weapon3
+        print(f"ครั้งที่ {i} hp left", monster)
+    elif weapon == 4:
+        monster = monster - weapon4
+        print(f"ครั้งที่ {i} hp left", monster)
+    else:
+        print("you want to use your own hand??? go back and choose again")
     i=i+1
 
     
